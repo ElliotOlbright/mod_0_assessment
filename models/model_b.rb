797087a31dc3d85a6cca900e3652653fabe5a2c7
Elliot Olbright <elliotolbright@gmail.com>
2021-04-23 11:54:06 -0600

    Add model_b.rb

---
+++ models/model_b.rb
@@ -0,0 +1,19 @@
+dog = {
+  'name' => 'Lady',
+  'city_born' => 'Tuscon',
+  'weight_in_lbs' => 27,
+  'age_in_months' => 7,
+  'is_healthy' => true,
+  'likes_to_swim' => false,
+  'favorite_toys' => ['stuffed bunny',
+    'kong with peanut butter',
+    'rope'
+  ],
+  'favorite_friends' => [
+    'max',
+    'pluto',
+    'atlas'
+  ]
+}
+
+puts dog.values
