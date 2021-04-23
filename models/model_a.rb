f93c161f6ea8f70e3e84a1f7d2c3f14a4affdcba
Elliot Olbright <elliotolbright@gmail.com>
2021-04-23 11:47:21 -0600

    Add model_a.rb

---
+++ models/model_a.rb
@@ -0,0 +1,12 @@
+UniverstyOfColorado = {
+  'city' => 'Denver',
+  'education_level' => 'college',
+  'number_of_students' => 8543,
+  'number_of_professors' => 122,
+  'has_instate_tuition' => true,
+  'has_financial_ help' => true,
+  'food_court' => ['McDonals', 'Pizza Hut', 'Jimmy Johns'],
+  'book_store_items' => ['notebooks', 'pens', 'calculators']
+}
+
+puts UniverstyOfColorado.keys
