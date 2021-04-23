f8b631824821505baafdabdfb5ff6cb7188f60e0
Elliot Olbright <elliotolbright@gmail.com>
2021-04-23 11:30:47 -0600

    Complete Program Specific Challange

---
+++ variables.rb
@@ -0,0 +1,16 @@
+famous_person = 'Gucci Mayne'
+is_active = true
+
+famous_person = "AOC"
+puts famous_person
+
+num3 = 3
+num9 = 9
+sum = num3 + num9
+puts sum
+
+animals = ['liger', 'charzard', 'hip-hop-oppotamus']
+animals.shift
+
+puts animals
+puts animals.length
